ac3f0d54da886e6c613ad8d69d9c41074c845df5
Buttertoes <kozinandriy@gmail.com>
2023-12-22 16:15:02 +1100

    Adding older Adiveda website

---
+++ app/config/parameters.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => '127.0.0.1',
+    'database_port' => '',
+    'database_name' => 'prestashop',
+    'database_user' => 'prestashop',
+    'database_password' => 'Wd87fh45nj82',
+    'database_prefix' => 'ps_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => 'gkt6fg0LKNCvSVJ592Phzg95FuMNrEj7DhwKWDSwyPdk2235o5xRftoz',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2019-10-23',
+    'locale' => 'en-US',
+    'cookie_key' => 'TOm8RokrLXKzCnUNoauchDs61qFe1BMycZ5GWe0qnkVGH70UfwmKTqM1',
+    'cookie_iv' => 'mv69V5wy',
+    'new_cookie_key' => 'def00000e553544a5610543420bcd605ca789a6a3327b359c7860852168021179ddf1ba4e5fa2bee4e9f587aeedd2f822012afea2552ab98a9dd86e99bfb27aa8f8f05a5',
+  ),
+);
